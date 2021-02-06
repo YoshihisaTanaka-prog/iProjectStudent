1a24a3c2684cb513d95280de70138610c0b10bca
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-02-06 21:13:18 +0900

    02/06 Yoshihisa pod 1st update commit

--- Podfile
+++ Podfile
@@ -5,9 +5,16 @@ target 'iProjectStudent' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for iProjectStudent
+  # Pods for iProjectStudent iProjectTeacher
+
+  pod 'SkyWay'
+
   pod 'NCMB', :git => 'https://github.com/NIFCLOUD-mbaas/ncmb_ios.git'
 	
   pod 'Cosmos'
+  
+  pod 'FSCalendar'
+  
+  pod 'CalculateCalendarLogic'
  
 end
--- Pods/Target Support Files/Pods-iProjectStudent/Pods-iProjectStudent-frameworks.sh
+++ Pods/Target Support Files/Pods-iProjectStudent/Pods-iProjectStudent-frameworks.sh
@@ -175,11 +175,15 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CalculateCalendarLogic/CalculateCalendarLogic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Cosmos/Cosmos.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar/FSCalendar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NCMB/NCMB.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CalculateCalendarLogic/CalculateCalendarLogic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Cosmos/Cosmos.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar/FSCalendar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NCMB/NCMB.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
