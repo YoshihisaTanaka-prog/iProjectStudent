096f46aa721b8bf1127aa2b6092b0975787801f0
knakamura17 <nakamura.kaori17@gmail.com>
2021-02-10 22:28:43 +0900

    2021/02/10_中村_アンケート画面余分なコードを消した

--- iProjectStudent/Questionnaire/Questionnaire_subViewController.swift
+++ iProjectStudent/Questionnaire/Questionnaire_subViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 import NCMB
 
-class QuestionnaireViewController: UIViewController,UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
-    
+class QuestionnaireViewController: UIViewController,UITextFieldDelegate, UIPickerViewDelegate {
+
     var questionaire: Questionnaire!
     let qustionlist = [[
         QuestionInputFormat(question: "応用よりもまずは基礎をじっくり学びたい", isNegative: true),
@@ -26,55 +26,17 @@ class QuestionnaireViewController: UIViewController,UITextFieldDelegate, UIPicke
         QuestionInputFormat(question: "絵や図で説明してもらう方がわかりやすい", isNegative: true),
         QuestionInputFormat(question: "自分の話をするより人の話を聞くことが多い", isNegative: false)
     ]]
-    
-//    @IBOutlet var Q1pickerView: UIPickerView!
-
-    var selected: String?
-    
-   let Q1dataList = ["Q1. 他人に自己紹介するのが苦手だと感じる。","あてはまる","ややあてはまる","あまりあてはまらない","あてはまらない"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         questionaire = Questionnaire(questions: qustionlist, size: getScreenSize(isExsistsNavigationBar: true, isExsistsTabBar: true)!, onlyEven: 4)
         
         self.view.addSubview(questionaire.mainScrollView)
-//        Q1pickerView.delegate = self
-//        Q1pickerView.dataSource = self
         
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    
-    // UIPickerViewの列の数
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    // UIPickerViewの行数、リストの数
-    func pickerView(_ pickerView: UIPickerView,
-                    numberOfRowsInComponent component: Int) -> Int {
-        return Q1dataList.count
-    }
-    
-    // UIPickerViewの最初の表示
-    func pickerView(_ pickerView: UIPickerView,
-                    titleForRow row: Int,
-                    forComponent component: Int) -> String? {
-        
-        return Q1dataList[row]
-    }
-    
-    // UIPickerViewのRowが選択された時の挙動
-    func pickerView(_ pickerView: UIPickerView,
-                    didSelectRow row: Int,
-                    inComponent component: Int) {
-        if row != 0 {
-            selected = Q1dataList[row]
-        } else {
-            selected = nil
-        }
-    }
-
+ 
 }
