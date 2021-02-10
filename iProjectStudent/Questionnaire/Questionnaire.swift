e6e6f27f15f593adb73de680d572dfaadc9ba1d5
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-02-10 19:48:44 +0900

    02/10 Yoshihisa add inter view 1st commit

--- iProjectStudent/Questionnaire/Questionnaire.swift
+++ iProjectStudent/Questionnaire/Questionnaire.swift
@@ -8,15 +8,17 @@
 
 import Foundation
 import UIKit
-
+import NCMB
 
 class Questionnaire {
     var mainScrollView = UIScrollView()
     var questionViews: [[QuestionView]] = []
     var totalNumbers: [Int] = []
     var result: Int = -1
+    
     init(questions: [[QuestionInputFormat]], size: Size, onlyEven numOfButton: Int) {
-        var mainSVHeight = 20.f // mainScrollViewの高さを計算する。
+        var mainSVHeight = 20.f // mainScrollViewの高さなどを設定するための変数
+//        アンケートの一番上に表示する文章の設定
         let titleLabel = UILabel(frame: CGRect(x: 10.f, y: 10.f, width: size.width - 20.f, height: 0))
         titleLabel.numberOfLines = 0
         titleLabel.text = """
@@ -29,18 +31,38 @@ class Questionnaire {
         mainSVHeight += titleLabel.frame.size.height
         
 //        質問の追加をする
+        for i in 0..<questions.count {
+            questionViews.append([])
+            self.totalNumbers.append(-1)
+            for j in 0..<questions[i].count {
+                questionViews[i].append(QuestionView(size, questions[i][j], numOfButton))
+                let qv = questionViews[i][j].mainView
+                let centerHight = mainSVHeight + qv.frame.size.height / 2.f
+                qv.center = CGPoint(x: size.width / 2.f, y: centerHight)
+                self.mainScrollView.addSubview(qv)
+                mainSVHeight += qv.frame.size.height + 10.f
+            }
+        }
         
-        mainSVHeight += 10.f
+//        「アンケートに答える」ボタンの設定
         let answerButton = UIButton(frame: CGRect(x: 0.f, y: mainSVHeight, width: size.width, height: 40))
-//        ボタンの設定をする
+        answerButton.setTitle("アンケート結果を送信", for: .normal)
+        answerButton.addTarget(self, action: #selector(tappedAnswer), for: .touchUpInside)
         self.mainScrollView.addSubview(answerButton)
         mainSVHeight += 50.f
+        
+//        スクロールビューの高さを指定
         self.mainScrollView.frame = CGRect(x: 0.f, y: 0.f, width: size.width, height: mainSVHeight)
     }
     
     @objc func tappedAnswer(){
-        if(isAnsweredAllQuestions()){}
-        else{}
+        if(isAnsweredAllQuestions()){
+//            無回答がある
+        }
+        else{
+//            reslt に値を埋め込む
+            self.mainScrollView.removeFromSuperview()
+        }
     }
     
     private func isAnsweredAllQuestions() -> Bool {
@@ -76,7 +98,6 @@ class QuestionView{
         questionLabel.text = question.question
         questionLabel.sizeToFit()
         let height = questionLabel.frame.size.height
-        self.mainView.frame = CGRect(x: 0.f, y: 0.f, width: size.width, height: height + 50.f)
         self.mainView.addSubview(questionLabel)
         
 //        選択肢の設定
@@ -88,13 +109,16 @@ class QuestionView{
         self.mainView.addSubview(label2)
         for i in 0..<numOfButton {
             self.buttons.append(UIButton(frame: CGRect(x: 0.f, y: 0.f, width: 30.f, height: 30.f)))
-            let buttonWidth = (size.width - 260.f) / 4.f
+            let buttonWidth = (size.width - 260.f) / numOfButton.f
             self.buttons[i].center = CGPoint(x: 130 + buttonWidth / 2.f + i.f * buttonWidth, y: height + 25)
             self.buttons[i].setTitle("○", for: .normal)
             self.buttons[i].tag = i
             self.buttons[i].addTarget(self, action: #selector(self.selected(_:)), for: .touchUpInside)
             self.mainView.addSubview(buttons[i])
         }
+        
+//        質問フォームの高さを指定
+        self.mainView.frame = CGRect(x: 0.f, y: 0.f, width: size.width, height: height + 50.f)
     }
     
     @objc func selected(_ sender: UIButton){
@@ -102,7 +126,12 @@ class QuestionView{
         for i in 0..<self.numOfButton {
             if(i == sender.tag){
                 buttons[i].setTitle("●", for: .normal)
-                self.result = i
+                if(isNegative){
+                    self.result = self.numOfButton - 1 - i
+                }
+                else{
+                    self.result = i
+                }
             }
             else{
                 buttons[i].setTitle("○", for: .normal)
@@ -125,7 +154,6 @@ class QuestionInputFormat{
         else{
             self.question = question + "?"
         }
-        
     }
 }
 
