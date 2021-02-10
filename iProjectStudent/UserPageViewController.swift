ba70089a7ea613a4201c367a1504783df8a58c55
knakamura17 <nakamura.kaori17@gmail.com>
2021-02-10 22:06:50 +0900

    アンケート質問文追加

---
+++ iProjectStudent/UserPageViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UserPageViewController.swift
+//  iProjectStudent
+//
+//  Created by Ring Trap on 2/10/21.
+//  Copyright © 2021 Tanaka_Yoshihisa_4413. All rights reserved.
+//
+
+import UIKit
+import NCMB
+
+class UserPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
