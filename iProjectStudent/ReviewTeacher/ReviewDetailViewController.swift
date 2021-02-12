d6b9911c434532261efabb3c29b3a6ed5a22f319
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-02-13 00:16:38 +0900

    02/12 Yoshihisa copy and add sum function in Review
    ファイル名がわかりにくいので、
    「CommentTeacherViewController.swift」から「ReviewDetailViewController.swift」に変更いたしました。

--- iProjectStudent/ReviewTeacher/ReviewDetailViewController.swift
+++ iProjectStudent/ReviewTeacher/ReviewDetailViewController.swift
@@ -10,45 +10,22 @@ import UIKit
 import NCMB
 import Cosmos
 
-class CommentTeacherViewController: UIViewController {
+class ReviewDetailViewController: UIViewController {
     @IBOutlet var ratingScore: CosmosView!
     @IBOutlet var commentBox: UITextView!
     @IBOutlet var sendButton: UIButton!
-    
+    @IBOutlet var titleField: UITextField!
     
     var isAbletoEdit: Bool!
-    var objectId: String?
     var numofBeforeScore: Double = 1
     var numofAfterScore: Double = 1
-    var isCreate: Bool = false
     var review: ReviewTeacher?
+    var teacherId: String?
+    var studentId: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //描画の設定
-        if objectId == nil{
-            isCreate = true
-            sendButton.setTitle("送信", for: .normal)
-        }else{
-            let query = NCMBQuery(className: "Review")
-            query?.whereKey("objectId", equalTo:objectId!)
-            query?.findObjectsInBackground({ (rating, error) in
-                if error == nil{
-                    let objects = rating as! [NCMBObject]
-                    let object = objects[0]
-                    self.review = ReviewTeacher(object)
-                    self.numofBeforeScore = self.review!.score
-                    self.ratingScore.rating = self.review!.score
-                    self.commentBox.text = self.review!.comment
-                }else {
-                    self.showOkAlert(title:"Alert", message:error!.localizedDescription)
-                    
-                }
-            })
-            
-        }
-        //描画の設定ここまで
-        
         // ここからコメントの編集設定をする
         
         // スター半分の評価ができるようにする(本人だけが編集できるよう設定してある）
@@ -64,14 +41,20 @@ class CommentTeacherViewController: UIViewController {
             sendButton.setTitle("編集不可", for: .normal)
             ratingScore.settings.updateOnTouch = false
         }
+        self.commentBox.isEditable = isAbletoEdit
+        self.titleField.isEnabled = isAbletoEdit
         
     }
+    
     @IBAction func sendReview(){
         if isAbletoEdit{
-            if isCreate {
+            if review != nil {
                 let object = NCMBObject(className: "Review")
                 object?.setObject(numofAfterScore, forKey: "Reviewscore")
-                //ここにコメントボックスの定義をする
+                object?.setObject(commentBox.text!, forKey: "Reviewcomment")
+                object?.setObject(titleField.text!, forKey: "Reviewtitle")
+                object?.setObject(self.studentId!, forKey: "studentId")
+                object?.setObject(self.teacherId!, forKey: "teacherId")
                 object?.saveInBackground({ (error) in
                     if error == nil{
                         self.dismiss(animated: true, completion: nil)
@@ -80,10 +63,17 @@ class CommentTeacherViewController: UIViewController {
                     }
                 })
                 
+                let object2 = NCMBObject(className: "TeacherParameter")
+                object2?.setObject(numofAfterScore, forKey: "averageScore")
+                object2?.setObject((numofAfterScore * 2).i, forKey: "totalScore")
+                object2?.setObject(1, forKey: "totalNum")
+                object2?.saveInBackground({ _ in})
                 
             }else{
-                let object = NCMBObject(className: "Review",objectId:self.objectId!)
+                let object = NCMBObject(className: "Review",objectId: self.review!.objectId)
                 object?.setObject(numofAfterScore, forKey: "Reviewscore")
+                object?.setObject(commentBox.text!, forKey: "Reviewcomment")
+                object?.setObject(titleField.text!, forKey: "Reviewtitle")
                 //コメントボックスのアップデートをする
                 object?.saveInBackground({ (error) in
                     if error == nil{
@@ -92,9 +82,15 @@ class CommentTeacherViewController: UIViewController {
                         self.showOkAlert(title:"エラー", message:error!.localizedDescription)
                     }
                 })
+                
+                let object2 = NCMBObject(className: "TeacherParameter", objectId: self.review!.teacherId)
+                var totalScore = object2?.object(forKey: "totalScore") as! Int
+                totalScore += ( (numofAfterScore - numofBeforeScore) * 2 ).i
+                let totalNum = object2?.object(forKey: "totalNum") as! Int
+                object2?.setObject(totalScore.d / totalNum.d / 2.d, forKey: "averageScore")
+                object2?.saveInBackground({ _ in})
+                
             }
         }
     }
-
-
 }
