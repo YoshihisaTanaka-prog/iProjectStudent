a9e772ca2ed366843a30f88a66924cc15862451a
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-02-13 21:54:43 +0900

    02/13 Yoshihisa commit

--- iProjectStudent/ReviewTeacher/ReviewDetailViewController.swift
+++ iProjectStudent/ReviewTeacher/ReviewDetailViewController.swift
@@ -48,49 +48,46 @@ class ReviewDetailViewController: UIViewController {
     
     @IBAction func sendReview(){
         if isAbletoEdit{
+            var object: NCMBObject?
             if review != nil {
-                let object = NCMBObject(className: "Review")
-                object?.setObject(numofAfterScore, forKey: "Reviewscore")
-                object?.setObject(commentBox.text!, forKey: "Reviewcomment")
-                object?.setObject(titleField.text!, forKey: "Reviewtitle")
-                object?.setObject(self.studentId!, forKey: "studentId")
-                object?.setObject(self.teacherId!, forKey: "teacherId")
-                object?.saveInBackground({ (error) in
-                    if error == nil{
+                object = NCMBObject(className: "Review")
+            }else{
+                object = NCMBObject(className: "Review",objectId: self.review!.objectId)
+            }
+            object?.setObject(numofAfterScore, forKey: "Reviewscore")
+            object?.setObject(commentBox.text!, forKey: "Reviewcomment")
+            object?.setObject(titleField.text!, forKey: "Reviewtitle")
+            object?.setObject(self.studentId!, forKey: "studentId")
+            object?.setObject(self.teacherId!, forKey: "teacherId")
+            object?.saveInBackground({ (error) in
+                if error == nil{
+                    if self.review != nil {
                         self.dismiss(animated: true, completion: nil)
                     }else{
-                        self.showOkAlert(title:"エラー", message:error!.localizedDescription)
+                        self.navigationController?.popViewController(animated: true)
                     }
-                })
-                
-                let object2 = NCMBObject(className: "TeacherParameter")
+                    
+                }else{
+                    self.showOkAlert(title:"エラー", message:error!.localizedDescription)
+                }
+            })
+            
+            let object2 = NCMBObject(className: "TeacherParameter")
+            if review != nil {
                 object2?.setObject(numofAfterScore, forKey: "averageScore")
                 object2?.setObject((numofAfterScore * 2).i, forKey: "totalScore")
                 object2?.setObject(1, forKey: "totalNum")
                 object2?.saveInBackground({ _ in})
-                
             }else{
-                let object = NCMBObject(className: "Review",objectId: self.review!.objectId)
-                object?.setObject(numofAfterScore, forKey: "Reviewscore")
-                object?.setObject(commentBox.text!, forKey: "Reviewcomment")
-                object?.setObject(titleField.text!, forKey: "Reviewtitle")
-                //コメントボックスのアップデートをする
-                object?.saveInBackground({ (error) in
-                    if error == nil{
-                        self.navigationController?.popViewController(animated: true)
-                    }else{
-                        self.showOkAlert(title:"エラー", message:error!.localizedDescription)
-                    }
-                })
-                
-                let object2 = NCMBObject(className: "TeacherParameter", objectId: self.review!.teacherId)
-                var totalScore = object2?.object(forKey: "totalScore") as! Int
-                totalScore += ( (numofAfterScore - numofBeforeScore) * 2 ).i
-                let totalNum = object2?.object(forKey: "totalNum") as! Int
-                object2?.setObject(totalScore.d / totalNum.d / 2.d, forKey: "averageScore")
+                object2?.setObject(numofAfterScore, forKey: "averageScore")
+                object2?.setObject((numofAfterScore * 2).i, forKey: "totalScore")
+                object2?.setObject(1, forKey: "totalNum")
                 object2?.saveInBackground({ _ in})
-                
             }
+            
         }
+        
     }
+    
+    
 }
