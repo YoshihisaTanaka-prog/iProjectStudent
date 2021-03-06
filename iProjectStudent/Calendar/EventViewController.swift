a9f3cdc81895aa129e7a034e3ff9bc0626c2903b
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-03-06 19:58:40 +0900

    03/06 Yoshihisa change pickerview in EventViewController.swift

--- iProjectStudent/Calendar/EventViewController.swift
+++ iProjectStudent/Calendar/EventViewController.swift
@@ -12,19 +12,21 @@ import NCMB
 class EventViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     //スケジュール内容入力テキスト
-    @IBOutlet var eventText: UITextView!
+    @IBOutlet private var eventText: UITextView!
     //日付フォーム(UIDatePickerを使用)
-    @IBOutlet var y: UIDatePicker!
+    @IBOutlet private var y: UIDatePicker!
     //日付表示
-    @IBOutlet var y_text: UILabel!
-    @IBOutlet var pickerView: UIPickerView!
-    @IBOutlet var studentLabel: UILabel!
+    @IBOutlet private var y_text: UILabel!
+    @IBOutlet private var pickerView: UIPickerView!
+    @IBOutlet private var subjectPickerView: UIPickerView!
+    @IBOutlet private var datePickerView: UIPickerView!
+    @IBOutlet private var studentLabel: UILabel!
     
     var teacher: User?
-    let formatter = DateFormatter()
-    
-    var subjectName = "教科を選択"
-    var subjectNameList = ["教科を選択","国語","数学","理科","社会","英語"]
+    private let formatter = DateFormatter()
+    private var subjectName = "教科を選択"
+    private var subjectNameList = ["教科を選択","国語","数学","理科","社会","英語"]
+    private var dateList = [[],[],["月","火","水","木","金","土","日"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,29 +39,74 @@ class EventViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         
         pickerView.dataSource = self
         pickerView.delegate = self
+        pickerView.tag = 1
+        subjectPickerView.dataSource = self
+        subjectPickerView.delegate = self
+        subjectPickerView.tag = 2
+        datePickerView.dataSource = self
+        datePickerView.delegate = self
+        datePickerView.tag = 3
         
         formatter.dateFormat = "yyyy/MM/dd"
         y_text.text = formatter.string(from: y.date)
+        
+        let date = Date()
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: date)
+        //日付の選択
+        for y in year-1..<year+3 {
+            dateList[0].append(y.s+"年")
+        }
+        for i in 1...12 {
+            dateList[1].append(i.s + "月")
+        }
+        for i in 0..<dateList[2].count {
+            dateList[2][i] = dateList[2][i] + "曜日"
+        }
         
     }
 
     
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return pickerView.tag
     }
     
+    //要修正
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return subjectNameList.count
+        switch pickerView.tag {
+        case 1:
+            return 0;
+        case 2:
+            return 0;
+        default:
+            return dateList[component].count
+        }
     }
-    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return subjectNameList[row]
+        switch pickerView.tag {
+        case 1:
+            return 0.s;
+        case 2:
+            return 0.s;
+        default:
+            return dateList[component][row]
+        }
     }
-
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+        default:
+            break
+        }
         subjectName = subjectNameList[row]
     }
+    
     
     //画面遷移(カレンダーページ)
     @IBAction func onbackClick(_: UIButton) {
@@ -67,11 +114,11 @@ class EventViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     }
 
     //日付フォーム
-    @IBAction func picker(){
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/MM/dd"
-        y_text.text = formatter.string(from: y.date)
-    }
+//    @IBAction func picker(){
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "yyyy/MM/dd"
+//        y_text.text = formatter.string(from: y.date)
+//    }
 
     //DB書き込み処理
     @IBAction func saveEvent(_ : UIButton){
