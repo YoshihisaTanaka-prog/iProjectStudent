e4de314734219b7603cabec1d14b1ca099829287
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-05-27 21:31:30 +0900

    ログインの判定とスケジュールの設定を更新

--- iProjectStudent/Calendar/CalendarViewController.swift
+++ iProjectStudent/Calendar/CalendarViewController.swift
@@ -41,22 +41,7 @@ class CalendarViewController: UIViewController, UITableViewDataSource, UITableVi
     
     override func viewWillAppear(_ animated: Bool) {
         
-        calenderView.appearance.headerDateFormat = "YYYY年M月"
-        calenderView.appearance.headerTitleColor = dColor.font
-        calenderView.calendarWeekdayView.weekdayLabels[0].text = "日"
-        calenderView.calendarWeekdayView.weekdayLabels[0].textColor = .red
-        calenderView.calendarWeekdayView.weekdayLabels[1].text = "月"
-        calenderView.calendarWeekdayView.weekdayLabels[1].textColor = dColor.font
-        calenderView.calendarWeekdayView.weekdayLabels[2].text = "火"
-        calenderView.calendarWeekdayView.weekdayLabels[2].textColor = dColor.font
-        calenderView.calendarWeekdayView.weekdayLabels[3].text = "水"
-        calenderView.calendarWeekdayView.weekdayLabels[3].textColor = dColor.font
-        calenderView.calendarWeekdayView.weekdayLabels[4].text = "木"
-        calenderView.calendarWeekdayView.weekdayLabels[4].textColor = dColor.font
-        calenderView.calendarWeekdayView.weekdayLabels[5].text = "金"
-        calenderView.calendarWeekdayView.weekdayLabels[5].textColor = dColor.font
-        calenderView.calendarWeekdayView.weekdayLabels[6].text = "土"
-        calenderView.calendarWeekdayView.weekdayLabels[6].textColor = .blue
+        calenderView.setToJapanise()
         
         loadEvent(selectedDate)
         let tmpCalendar = Calendar(identifier: .gregorian)
